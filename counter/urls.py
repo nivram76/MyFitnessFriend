1e40cd684d57c5c2bc425e3d80760ebf2738ebb2
Marvin x <marvinxu2004@gmail.com>
2024-05-16 16:08:13 -0400

    forums work added

--- counter/urls.py
+++ counter/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.foodcalc, name='foodcalc'),
     path('tdee', views.tdee, name='tdee'),
+    path('room/<str:pk>/', views.room, name= 'room'),
+    path('forums', views.forums, name= 'forums')
 ]
--- counter/views.py
+++ counter/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from.tdeeform import TDEEForm
+import re
+
 
 # Create your views here.
 
 #API KEY: u5vq9UaMtsQa6ItWSEISiA==jJ9dmq2jaz4bkpXV
+
+
+# Room IDs:
+rooms= [
+    {'id': 1, 'name': 'Weight Training' },
+    {'id': 2, 'name': 'Sports' },
+    {'id': 3, 'name': 'Nutrition' }
+]
+
+
+
+
+
+
+
+
 
 #homepage req
 def foodcalc(request): 
@@ -16,15 +34,21 @@ def foodcalc(request):
         api_request=requests.get(api_url , headers= {'X-Api-Key': 'u5vq9UaMtsQa6ItWSEISiA==jJ9dmq2jaz4bkpXV'}) #sends get req to apininjas to get the nutritional info
 
         try:
-            api= json.loads(api_request.content) #Processes the content received from API and converts it to a python object
-        except Exception as e:
+            api= json.loads(api_request.content)[0] #Processes the content received from API and converts it to a python object
+            api["serving"]= '100g ' + api["name"] # setting serving size to default 100g
+            api["name"]= query
+            
+            if re.search("\d", query): #it is a valid food and the input has a # it means there is a custom serving size so we adjust
+                api["serving"]= query
+        except Exception as e: #input not recognized by api, ie. not a food 
                 api = "Error"
                 print(e)
-        return render(request, 'foodcalc.html', {'api': api})
+        return render(request, 'counter/foodcalc.html', {'api': api})
     else:
-        return render(request, 'foodcalc.html', {'query': 'Enter a valid query'})
+        return render(request, 'counter/foodcalc.html', {'query': 'Enter a valid query'})
     
 
+#tdee calc
 def tdee(request):
     if request.method == 'POST':
         form= TDEEForm(request.POST) 
@@ -40,4 +64,20 @@ def tdee(request):
         form= TDEEForm()
         result= 0
 
-    return render(request, 'tdee.html', {'form': form, 'result': result})
+    return render(request, 'counter/tdee.html', {'form': form, 'result': result})
+
+
+
+def forums(request):
+    context= {'rooms': rooms}
+    return render(request, 'counter/forums.html', context)
+
+def room(request, pk):
+    room= None
+
+    for r in rooms:
+        if r['id'] == int(pk):
+            room= r
+
+    context= {'room': room}
+    return render(request, 'counter/room.html', context)
